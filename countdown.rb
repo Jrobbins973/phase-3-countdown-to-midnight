49f0b0ee37eb0da25d2cc509ae4ab5b9f083a0c3
Joe Robbins <joerobbins@Joes-MacBook-Air.local>
2022-11-01 08:27:41 -0400

    commit tree 8c5e578dd321096f8b4aa5da8c056ca347070c45

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown(x = 10)
+    
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        # sleep 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep x = 5
+    while x > 0
+        puts  puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep 1
+    end
+end
+
+countdown_with_sleep
